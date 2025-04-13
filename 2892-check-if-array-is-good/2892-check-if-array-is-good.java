1a3cdb1770a597d90200d9e42aff7292eff124c6
Parna Roy Chowdhury <123104247+ParnaRoyChowdhury777@users.noreply.github.com>
2025-04-14 00:58:58 +0530

    Time: 3 ms (56.47%), Space: 42.5 MB (95.04%) - LeetHub

---
+++ 2892-check-if-array-is-good/2892-check-if-array-is-good.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isGood(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+
+        for(int i=0; i<n-1; i++) {
+            if(nums[i] != i+1) {
+                return false;
+            }
+        }
+
+        if(nums[n - 1] != n-1) {
+            return false;
+        } 
+        return true;
+        
+    }
+}
